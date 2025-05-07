84108e03fdb745899c3c1c813263615b66db9a19
OwenRW13 <owassel36@gmail.com>
2025-05-06 20:27:31 -0400

    Update SSTable.R

--- SSTable.R
+++ SSTable.R
@@ -1,27 +1,36 @@
+# Load required libraries
 library(tidyverse)
 library(rvest)
 library(dplyr)
+library(knitr)
+# Scrape NL Silver Slugger Data from Baseball reference
 SSNLRawList <- read_html(x = "https://www.baseball-reference.com/awards/silver_slugger_nl.shtml") %>%
   html_elements(css = "table") %>%
   html_table()
 SSNLData <- bind_rows(SSNLRawList)
+# Extract 2019 year from 'Year & Common' column
 SSNLData <- SSNLData %>%
   mutate(Year = str_extract(`Year & Common`, "^\\d{4}"))
 SSNL2019 <- SSNLData %>%
   filter(Year == "2019")
+# Scrape AL Silver Slugger Data from Baseball Reference
 SSALRawList <- read_html(x = "https://www.baseball-reference.com/awards/silver_slugger_al.shtml") %>%
   html_elements(css = "table") %>%
   html_table()
 SSALData <- bind_rows(SSALRawList)
+# Extract 2019 year from 'Year & Common' column
 SSALData <- SSALData %>%
   mutate(Year = str_extract(`Year & Common`, "^\\d{4}"))
 SSAL2019 <- SSALData %>%
   filter(Year == "2019")
+# Label leagues
 SSNL2019 <- SSNL2019 %>%
   mutate(League = "NL")
 SSAL2019 <- SSAL2019 %>%
   mutate(League = "AL")
+# Combine AL and NL data
 SS2019 <- bind_rows(SSNL2019, SSAL2019)
+# Reshape data from wide to long format for positions
 SS2019_long <- SS2019 %>%
   pivot_longer(
     cols = c("P", "C", "1B", "2B", "3B", "SS", "OF...7", "OF...8", "OF...9", "OF...10", "DH", "Utility"),
@@ -32,11 +41,16 @@ SS2019_long <- SS2019 %>%
   mutate(Position = as.character(Position),   
          Position = ifelse(grepl("^OF", Position), "OF", Position)) %>% 
   select(Year, League, Player, Position)
+# Load 2019 batters data from baseball savant
 batters2019 <- `2019.batters`
+# Rename player column for consistency and ease when merging data frames
 colnames(batters2019)[colnames(batters2019) == "last_name..first_name"] <- "Player"
+# Drop unnecessary columns
 batters2019 <- batters2019 %>%
   select(-c(player_id, team_id, pa, pitches))
+# Convert player names to characters to edit 
 SS2019_long$Player <- as.character(SS2019_long$Player)
+# Manually edit names to match the data from baseball savant
 SS2019_long$Player[grepl("Greinke", SS2019_long$Player)] <- "Greinke, Zack"
 SS2019_long$Player[grepl("Realmuto", SS2019_long$Player)] <- "Realmuto, J.T."
 SS2019_long$Player[grepl("Freeman", SS2019_long$Player)] <- "Freeman, Freddie"
@@ -55,6 +69,9 @@ SS2019_long$Player[grepl("Trout", SS2019_long$Player)] <- "Trout, Mike"
 SS2019_long$Player[grepl("Betts", SS2019_long$Player)] <- "Betts, Mookie"
 SS2019_long$Player[grepl("Springer", SS2019_long$Player)] <- "Springer, George"
 SS2019_long$Player[grepl("Cruz", SS2019_long$Player)] <- "Cruz, Nelson"
+# Merge data frames to display each silver slugger and their run values
 merged_data <- inner_join(SS2019_long, batters2019, by = "Player")
 merged_data <- merged_data %>%
   select(-c(year))
+# Create table to show relationships
+kable(merged_data)
