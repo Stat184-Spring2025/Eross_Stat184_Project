2a5fce304086a95ee7ba1121bb2092b5daf25b7c
OwenRW13 <owassel36@gmail.com>
2025-05-06 21:05:19 -0400

    Update SSGraphs.R

--- SSGraphs.R
+++ SSGraphs.R
@@ -1,5 +1,8 @@
+# Load ggplot2 library for graph making
 library(ggplot2)
+# adjust data name for simplicity
 batters <- merged_data
+# Graph displaying relationship between total runs and waste zone runs
 ggplot(batters) +
   aes(x = runs_all, y = runs_waste, label = Player) +
   geom_label() +
@@ -10,6 +13,7 @@ ggplot(batters) +
   ) +
   theme_minimal() +
   theme(plot.title = element_text(hjust = 0.5))
+# Graph displaying relationship between total runs and shadow zone runs
 ggplot(batters) +
   aes(x = runs_all, y = runs_shadow, label = Player) +
   geom_label() +
@@ -20,16 +24,18 @@ ggplot(batters) +
   ) +
   theme_minimal() +
   theme(plot.title = element_text(hjust = 0.5))
+# Graph displaying relationship between total runs and chase zone runs
 ggplot(batters) +
-  aes(x = runs_all, y = runs_waste, label = Player) +
+  aes(x = runs_all, y = runs_chase, label = Player) +
   geom_label() +
   labs(
     x = "Total Runs",
-    y = "Waste Zone Runs",
-    title = "Total Runs vs. Waste Runs"
+    y = "Chase Zone Runs",
+    title = "Total Runs vs. Chase Runs"
   ) +
   theme_minimal() +
   theme(plot.title = element_text(hjust = 0.5))
+# Graph displaying relationship between total runs and heart zone runs
 ggplot(batters) +
   aes(x = runs_all, y = runs_heart, label = Player) +
   geom_label() +
